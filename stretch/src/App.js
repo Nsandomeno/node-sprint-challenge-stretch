1e8902267c0ea55efc59bc0285f8ff611c41b190
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-02-14 13:36:22 -0500

    stretch done

---
+++ stretch/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/projects')
+      .then((response) => {
+        console.log("This is the response:", response)
+      })
+      .catch((error) => {
+        console.log("This is the error:", error)
+      })
+  }, [])
+  
+  return (
+    <div className="App">
+      {
+        projects.map((project) => {
+          return (
+            <div key={project.id}>
+              <h1> {project.name} </h1>
+              <h3> {project.description} </h3>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
+
